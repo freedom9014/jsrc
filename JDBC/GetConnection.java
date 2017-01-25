f4cb404ab1ca1ed596ab8e36b5c97f23d0467376
freedom9014 <freedom9014@gmail.com>
2017-01-25 15:18:32 +0900

    コメントの追加

--- JDBC/GetConnection.java
+++ JDBC/GetConnection.java
@@ -9,17 +9,20 @@ class GetConnection{
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 
 			//Oracleに接続する
-				Connection=
+				Connection cn =
 			DriverManager.getConnection
-	 	 	 ("jdbc:oracle:thin:@localhost:1521:ip",	"scott","tiger");
+			//接続文字列。環境に応じてカスタム
+	 	 	 ("jdbc:oracle:thin:@localhost:1521:orcl",	"scott","tiger");
 			System.out.println("接続完了");
 
-			//Oracleから切断する
+			//Oracleから切断する。切断しないとメモリ開放が明示的にされない。
 			cn.close();
 			System.out.println("切断完了");
 				
+		//JDBCを含むjarファイルにクラスパスを通し忘れると発生。
 		}catch(ClassNotFoundException e){
 			e.printStackTrace();
+		//OracleとのSQLでのやり取りでエラーが発生すると例外。
 		}catch(SQLException e){
 			e.printStackTrace();
 		}catch(Exception e){
