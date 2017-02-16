9b1890318ade4758fb2ebb80ae6ce5535aaf6ebf
freedom9014 <freedom9014@gmail.com>
2017-02-16 10:44:52 +0900

    さいとう
    さいとう

---
+++ testtt/test.js
@@ -0,0 +1,11 @@
+function formSubmit() {
+document.write("<form method=post action=");
+document.write("/cgi-bin/my_script");
+document.write(">");
+document.write("–¼‘O‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+document.write("<br>");
+document.write("<input type=text value=");
+document.write("?????");
+document.write(">");
+alert(document.forms.length);
+}
